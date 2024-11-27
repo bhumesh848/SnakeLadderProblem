0331c412c5ea281ae7c6e858a99c43a9afda9178
Mac02 <mac02@Mac02s-MacBook-Pro.local>
2024-11-27 14:57:16 +0530

    Player 1 rolls a dice and results between 1 to 6 in Uc2

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -7,6 +7,10 @@ public static final int WIN_POSITION = 100;
         int player1Position = 0;
         System.out.println("Start the snake ladder game");
         System.out.println("Player 1 is at position " + player1Position);
+        System.out.println("Player 1 rolls a dice");
+        int rolledResult = (int)(Math.random()* 6)+1;
+        System.out.println("Player 1 rolls a dice & result is "+rolledResult);
+
 
 
 
