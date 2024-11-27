7b9d89f9a9bfe92ed6c241285e5d805b95a50409
Mac02 <mac02@Mac02s-MacBook-Pro.local>
2024-11-27 14:31:26 +0530

    Uc1 Started the snake ladder game with single player

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+public static final int WIN_POSITION = 100;
+
+    public static void main(String[] args) {
+        int player1Position = 0;
+        System.out.println("Start the snake ladder game");
+        System.out.println("Player 1 is at position " + player1Position);
+
+
+
+
+    }
+}
